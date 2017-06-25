5fe17bfeabf42ffecb88e5c1a186f6f4996841cd
Isabella Maki <ikmaki14@gmail.com>
2017-06-25 16:20:24 +0000

    added links to buttons

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,18 @@
 class TodoController < ApplicationController
+    # list all todos
     def index
     end
+    
+    # display form to create new todo
     def new
     end
+    
+    # display form to edit existing todo
+    def edit
+       @todo=Todo.find_by_id(params[:id]) 
+    end
+    
+    # save todo to database
     def create
         t=Todo.new
         t.description=params[:description]
@@ -10,31 +20,9 @@ class TodoController < ApplicationController
         t.save
         redirect_to "/todo/show/#{t.id}"
     end
+   
+    # display specific todo 
     def show
         @todo=Todo.find_by_id(params[:id])
-        # todo_id = params[:id]
-        
-        # if todo_id == '1'
-        #     @pomodoro_estimate=4
-        #     @pomodoro_description="Make the curriculum"
-        # elsif todo_id == '2'
-        #     @pomodoro_estimate=3
-        #     @pomodoro_description="Buy workshop supplies"
-        # elsif todo_id == '3'
-        #     @pomodoro_estimate=2
-        #     @pomodoro_description="Meet with volunteer trainers"
-        # elsif todo_id == '4'
-        #     @pomodoro_estimate=1
-        #     @pomodoro_description="Order food for Saturday and Sunday"
-        # elsif todo_id == '5'
-        #     @pomodoro_estimate=2
-        #     @pomodoro_description="Check pre-work assignments"
-        # elsif todo_id == '6'
-        #     @pomodoro_estimate=1
-        #     @pomodoro_description="Send workshop location to all students"
-        # elsif todo_id == '7'
-        #     @pomodoro_estimate=0
-        #     @pomodoro_description="Have a great workshop"
-        # end
     end
 end

8816cba2dc21ed83c5f2bf1a714525ee69d9e7de
Isabella Maki <ikmaki14@gmail.com>
2017-06-25 16:46:32 +0000

    edit todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,6 +7,14 @@ class TodoController < ApplicationController
     def new
     end
     
+    def update
+        t=Todo.find_by_id(params[:id])
+        t.description=params[:description]
+        t.pomodoro_estimate=params[:pomodoro_estimate]
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    
     # display form to edit existing todo
     def edit
        @todo=Todo.find_by_id(params[:id]) 
